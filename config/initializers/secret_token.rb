892b1c04fc1b7a9a10e08d9beb20ee0f17501ec8
Dis <sr_5@inbox.ru>
2013-09-22 16:19:43 +0700

    HTML5 Form Helpers

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CatchUpWithRor4::Application.config.secret_key_base = 'fdce15b5a7e06f31d585d9005ab9c7e765dbd45d7b940129285f6e53164d2dcd75f2368b120b6c699402b100074e2dc672235666311322d6391ec8bef5eb2391'
